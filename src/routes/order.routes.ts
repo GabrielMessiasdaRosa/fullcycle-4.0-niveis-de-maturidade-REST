e3a1db9a9ff6f36755abeec1cda9b13547ac6b0f
Gabriel Messias da Rosa <gabrielmessiasdarosa@gmail.com>
2025-03-04 22:26:59 +0000

    feat: :sparkles: refatora rotas e serviços para utilizar UUIDs, melhora a estrutura do código e adiciona configurações do VSCode

--- src/routes/order.routes.ts
+++ src/routes/order.routes.ts
@@ -3,18 +3,16 @@ import { createOrderService } from '../services/order.service';
 
 const router = Router();
 
-router.post('/createOrder', async (req, res) => {
+router.post('/', async (req, res) => {
     const orderService = await createOrderService();
-    const { payment_method, card_token } = req.body;
-    // @ts-expect-error
-    const { cart_id } = req.session;
+    const { payment_method, card_token, cart_uuid } = req.body;
     // @ts-expect-error
     const customerId = req.userId;
-    const { order, payment } = await orderService.createOrder({customerId, payment_method, card_token, cart_id});
+    const { order, payment } = await orderService.createOrder({customerId, payment_method, card_token, cart_uuid});
     res.json({ order, payment });
 });
 
-router.get('/listOrders', async (req, res) => {
+router.get('/', async (req, res) => {
     const orderService = await createOrderService();
     const { page = 1, limit = 10 } = req.query;
     // @ts-expect-error
